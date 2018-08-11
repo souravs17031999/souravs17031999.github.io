a5dd0541a33214127c7057b776005d9de6d18bbe
sourav kumar <souravs_1999@rediffmail.com>
2018-08-11 12:15:28 +0530

    115

--- portfolio1.js
+++ portfolio1.js
@@ -15,7 +15,21 @@
 		document.body.scrollTop = 0; // For Safari
 		document.documentElement.scrollTop = 0;} // For Chrome, Firefox, IE and Opera 
 
-
+	function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 
 		
 		
