9214341c5be3b067bb4012aa75276fc3126f42da
sourav kumar <souravs_1999@rediffmail.com>
2018-08-17 14:22:59 +0530

    136

--- portfolio1.js
+++ portfolio1.js
@@ -1,21 +1,6 @@
 
-	// When the user scrolls down 20px from the top of the document, show the button
 
-	window.onscroll = function() {scrollFunction()};
-
-	function scrollFunction() {
-		if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-			document.getElementById("myBtn").style.display = "block";} 	
-		else {
-			document.getElementById("myBtn").style.display = "none";}
-		}
-
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-		document.body.scrollTop = 0; // For Safari
-		document.documentElement.scrollTop = 0;} // For Chrome, Firefox, IE and Opera 
-
-	function startTime() {
+function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
@@ -28,8 +13,15 @@
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
+return i;}
 
-		
-		
+
+
+
+$(document).ready(function(){
+
+   // jQuery methods go here...
+
+
+$(window).scroll(function(){if($(this).scrollTop()>200){$('#scrollTopBtn').stop().fadeIn()}else{$('#scrollTopBtn').stop().fadeOut()}});$('#scrollTopBtn').click(function(){$('html, body').animate({scrollTop:0},600);return!1});
+});
