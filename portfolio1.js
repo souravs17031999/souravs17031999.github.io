07aac1c521e23d5d59c74b3883992bc83ef09065
sourav kumar <souravs_1999@rediffmail.com>
2018-09-16 15:07:55 +0530

    198.1

--- portfolio1.js
+++ portfolio1.js
@@ -37,18 +37,3 @@ $(document).ready(function() {
     });
 
 });
-
-$(document).ready(function(){
-  var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:3,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true
-	
-	
-	
-});
-});
